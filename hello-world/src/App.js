e9dd7982c6dab893842725b1997548601b2ed1bb
Bambyboi <[samuelkaleopa9@gmail.com]>
2025-03-26 22:42:29 +1300

    commit

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
 import { Component } from 'react';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Hello2 from './components/Hello';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Greet />
+        <Greet name='Samuel' heroName='Aquaman'/>
+        <Greet name='Jessop' heroName='Spiderman'/>
+        <Greet name='Kaleopa' heroName='Ladyboy'/>
+
+        {/* <Welcome /> 
+        <Hello /> */}
+   
       </div>
     )
   }
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
-function Greet(){
-    return <h1>
-        Hello Samuel
-    </h1>
+// this component is for an example of function component
+//_______________________________________________________
+// function Greet(){
+//     return <h1>
+//                 Hello Samuel
+//             </h1>
+// }
+
+//const Greet = () => <h1>"Hello Samuel"</h1>
+
+// including properties
+ const Greet = props => {
+    console.log(props)
+  return <h1>Hello My name {props.name}</h1>
 
 }
+
+
 
 export default Greet
